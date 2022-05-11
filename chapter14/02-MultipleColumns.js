2b4773bdd593d912e30b120f6c1042eeb91c2a87
Jorge Ballote <ealjkj@gmail.com>
2022-05-10 19:04:18 -0500

    second feedback adressed

--- chapter14/02-MultipleColumns.js
+++ chapter14/02-MultipleColumns.js
@@ -2,7 +2,9 @@ const layoutSelectionElement = document.getElementById('layout-selection');
 const containerElement = document.getElementById('container');
 layoutSelectionElement.addEventListener('click', function changeLayout(e) {
     let btn = e.target.closest("button");
-    if(btn){
+    if(btn){    
+        document.body.removeChild(containerElement);
         containerElement.style.columnCount = btn.getAttribute("data-columns");
+        document.body.appendChild(containerElement);
     }
 })
